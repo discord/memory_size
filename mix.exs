a3effd8c1bfa38cc4cece9be54aa81f8bc8ea2fe
Yuliy Pisetsky <yuliy.pisetsky@discordapp.com>
2023-09-12 00:23:43 +0000

    add hex package info

--- mix.exs
+++ mix.exs
@@ -21,4 +21,17 @@ defmodule MemorySize.MixProject do
       {:ex_doc, "~> 0.30", only: :dev, runtime: false}
     ]
   end
+
+  def package do
+    [
+      name: :memory_size,
+      description: "A small library for estimating the memory usage of huge process states.",
+      files: ["lib", ".formatter.exs", "README.md", "LICENSE", "mix.exs"],
+      maintainers: ["Discord Realtime Infrastructure"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/discord/memory_size",
+      },
+    ]
+  end
 end
